27bce7520a222aeee7aac848c3ce11dd37710143
bubblubskiii <paulose.aiden07@gmail.com>
2025-09-22 18:29:25 +0100

    Add project files.

---
+++ InteractiveConwaysGameOfLife/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace InteractiveConwaysGameOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.btnStepByOne = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnResetGrid = new System.Windows.Forms.Button();
+            this.btnSaveGrid = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.Color.White;
+            this.gamePanel.Location = new System.Drawing.Point(35, 21);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(401, 401);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.gamePanel_Paint);
+            this.gamePanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gamePanel_MouseClick);
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Location = new System.Drawing.Point(497, 138);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(250, 50);
+            this.btnStartGame.TabIndex = 1;
+            this.btnStartGame.Text = "start";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // btnStepByOne
+            // 
+            this.btnStepByOne.Location = new System.Drawing.Point(497, 82);
+            this.btnStepByOne.Name = "btnStepByOne";
+            this.btnStepByOne.Size = new System.Drawing.Size(250, 50);
+            this.btnStepByOne.TabIndex = 0;
+            this.btnStepByOne.Text = "step";
+            this.btnStepByOne.UseVisualStyleBackColor = true;
+            this.btnStepByOne.Click += new System.EventHandler(this.btnStepByOne_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(497, 194);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(250, 50);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnResetGrid
+            // 
+            this.btnResetGrid.Location = new System.Drawing.Point(497, 250);
+            this.btnResetGrid.Name = "btnResetGrid";
+            this.btnResetGrid.Size = new System.Drawing.Size(250, 50);
+            this.btnResetGrid.TabIndex = 3;
+            this.btnResetGrid.Text = "reset";
+            this.btnResetGrid.UseVisualStyleBackColor = true;
+            this.btnResetGrid.Click += new System.EventHandler(this.btnResetGrid_Click);
+            // 
+            // btnSaveGrid
+            // 
+            this.btnSaveGrid.Location = new System.Drawing.Point(497, 306);
+            this.btnSaveGrid.Name = "btnSaveGrid";
+            this.btnSaveGrid.Size = new System.Drawing.Size(250, 50);
+            this.btnSaveGrid.TabIndex = 4;
+            this.btnSaveGrid.Text = "save";
+            this.btnSaveGrid.UseVisualStyleBackColor = true;
+            this.btnSaveGrid.Click += new System.EventHandler(this.btnSaveGrid_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveGrid);
+            this.Controls.Add(this.btnResetGrid);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStepByOne);
+            this.Controls.Add(this.btnStartGame);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "Form1";
+            this.Text = "Conways Game of LIfe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Button btnStepByOne;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnResetGrid;
+        private System.Windows.Forms.Button btnSaveGrid;
+    }
+}
+
---
+++ InteractiveConwaysGameOfLife/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InteractiveConwaysGameOfLife
+{
+    public partial class Form1 : Form
+    {
+
+        private const int gridSize = 10;
+        private const int cellSize = 40;
+        
+        // main array that holds the cells 
+        public  int[,] gameArray = new int[gridSize, gridSize];
+        public int[,] gameArrayDupe = new int[gridSize, gridSize];
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            gamePanel.Paint += gamePanel_Paint;
+
+            // prevents any flickering
+          //  gamePanel.DoubleBuffered(true);
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void MainGame()
+        {
+            // a temp array to store the next iteration's cells
+            int[,] workingArray = new int[10, 10];
+            workingArray = FillArrayWithZeros(10, 10);
+
+
+            // looping through each cell in the gameArray
+
+            for (int row = 0; row < gameArray.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < gameArray.GetLength(1) - 1; col++)
+                {
+                    int neighbours = ReturnNeighbours(gameArray, row, col);
+
+                    //Console.WriteLine(neighbours);
+
+
+                    // checks if the cell is alive, and then applies conway's rules 
+                    if (gameArray[row, col] == 1)
+                    {
+                        // if cell has either 2 or 3 neighbouring cells then it will live
+                        if ((neighbours == 2) || (neighbours == 3))
+
+                        {
+                            workingArray[row, col] = 1;
+                        }
+                        else
+                        {
+                            workingArray[row, col] = 0;
+                        }
+                    }
+                    // applying conways rules to the dead cells
+                    else
+                    {
+                        if (neighbours == 3)
+                        {
+                            workingArray[row, col] = 1;
+                        }
+                    }
+
+                }
+            }
+
+            gameArray = workingArray;
+
+           // Thread.Sleep(250);
+
+            // refreshing the panel
+            gamePanel.Invalidate();
+        }
+
+        static int ReturnNeighbours(int[,] gameArray, int row, int col)
+        {
+            int neighbours = 0;
+
+            int rowLimit = gameArray.GetLength(0) - 1;
+            int colLimit = gameArray.GetLength(1) - 1;
+
+
+
+            // Min and Max is used to find out whether the neighbouring value will be in boundary of array
+            // like a typewriter 
+            for (int y = Math.Max(0, row - 1); y <= Math.Min(rowLimit, row + 1); y++)
+            {
+                for (int x = Math.Max(0, col - 1); x <= Math.Min(colLimit, col + 1); x++)
+                {
+
+                    // outputs everything except the centre cell
+                    if (x != col || y != row)
+                    {
+
+                        if (gameArray[y, x] == 1)
+                        {
+
+                            neighbours++;
+                            //Console.WriteLine("(" + col + "," + row + ")" + neighbours);
+                        }
+                    }
+                }
+            }
+
+            return neighbours;
+        }
+
+        static int[,] FillArrayWithZeros(int numRows, int numCols)
+        {
+
+            int[,] temp = new int[numRows, numCols];
+
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    temp[i, j] = 0;
+                }
+            }
+
+            return temp;
+        }
+
+
+
+
+        private void gamePanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    Rectangle rect = new Rectangle(col * cellSize, row * cellSize, cellSize, cellSize); // creating a graphics obj for each cell
+                    graphics.DrawRectangle(Pens.Black, rect); // grid outline
+
+                    // if cell is alive colouring it in
+                    if (gameArray[row, col] == 1)
+                    {
+                        graphics.FillRectangle(Brushes.LightSteelBlue, rect);
+                        graphics.DrawRectangle(Pens.Black, rect);
+                    }
+                }
+            }
+        }
+
+
+        // detects input from user and then either turns a cell on or off depending on its current state
+        private void gamePanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            int row = e.Y / cellSize;
+            int col = e.X / cellSize;
+
+            if(row >= 0 && row < gridSize && col >= 0 && col < gridSize)
+            {
+                if (gameArray[row, col] == 0)
+                {
+                    gameArray[row, col] = 1;
+                }
+                else
+                {
+                    gameArray[row, col] = 0;
+                }
+
+            }
+
+            Console.WriteLine($"Clicked row={row}, col={col}, state={gameArray[row, col]}");
+
+            gamePanel.Invalidate();
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+           
+        }
+
+        private void btnStepByOne_Click(object sender, EventArgs e)
+        {
+            MainGame();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // each tick 
+            MainGame();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+
+
+        private void btnResetGrid_Click(object sender, EventArgs e)
+        {
+            gameArray = gameArrayDupe;
+            gamePanel.Invalidate();
+        }
+
+        private void btnSaveGrid_Click(object sender, EventArgs e)
+        {
+            gameArrayDupe = gameArray;
+        }
+    }
+
+
+
+}
